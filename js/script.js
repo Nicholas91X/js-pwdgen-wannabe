7a04e73acc4bd852f314f227ef7d4dfa5e0f88fc
Nicholas <romitinicholas@gmail.com>
2021-11-04 18:12:09 +0100

    ultima versione

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 // alert('Prova')
 
-let nome = prompt("Inserisci il tuo nome");
-
-let cognome = prompt("Inserisci il tuo cognome");
-
-let colore = prompt("Inserisci il tuo colore preferito");
-
-let myPassword = nome + cognome + colore + 21;
-// console.log(myPassword);
-
-document.getElementById("my_Password").innerHTML = myPassword;
+my_Password.onclick = function(){
+    let nome = prompt("Inserisci il tuo nome");
+    let cognome = prompt("Inserisci il tuo cognome");
+    let colore = prompt("Inserisci il tuo colore preferito");
+    let myPassword = nome + cognome + colore + 21;
+    document.getElementById("my_Password").innerHTML = myPassword;
+};
